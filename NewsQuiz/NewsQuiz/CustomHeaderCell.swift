9e0566123a76c7a0d4b4b92de173d0262b7c658f
kokowin18 <kkwp4b@mail.missouri.edu>
2015-05-12 15:56:37 -0500

    Nothing

---
+++ NewsQuiz/NewsQuiz/CustomHeaderCell.swift
@@ -0,0 +1,25 @@
+//
+//  CustomHeaderCell.swift
+//  NewsQuiz
+//
+//  Created by University of Missouri on 5/7/15.
+//  Copyright (c) 2015 Thomas Henry Riddick. All rights reserved.
+//
+
+import UIKit
+
+class CustomHeaderCell: UITableViewCell {
+    @IBOutlet weak var headerLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
